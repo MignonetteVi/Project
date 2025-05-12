ec81f19faade3a7e16e7ed6af36fbe7ce1825b94
Aziz-is-a-student <azizrasidov99@gmail.com>
2025-05-12 14:35:56 +0500

    Check Git Desktop

--- core/src/main/java/com/celestialwarfront/game/FirstScreen.java
+++ core/src/main/java/com/celestialwarfront/game/FirstScreen.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 
-/** Первый экран игры. Использует GameManager для всей игровой логики */
+
 public class FirstScreen implements Screen {
     private GameManager gameManager;
 
